b744e4bac3eb1cca09e9af59761cd6e4058800b1
Sommai-Supawong <Somai.050449@gmail.com>
2026-06-25 17:02:27 +0700

    fix: update server port to 8888 and improve console log message

--- src/sever.js
+++ src/sever.js
@@ -1,12 +1,13 @@
 // index.js — Express.js Server (Preview)
 import express from 'express';
+import "dotenv/config"; // Load environment variables from .env file
 const app = express();
-const PORT = 3000;
+const PORT = 8888;
 app.use(express.json()); // Middleware: แปลง JSON
 // Routes
 app.get('/', (req, res) => {
 res.json({ message: 'สวัสดีจาก Express.js!' });
 });
 app.listen(PORT, () => {
-console.log("Server is running on http://localhost:3000");
+console.log(`Server is running on http://localhost:${PORT}`);
 });
